7d97a731b897549bd50f7a4f369ae8102d2b8a50
bklynate <nathanielcfa@gmail.com>
2015-02-23 17:29:20 -0500

    Using 'attr_accessor' to write getter/setter methods all at once. Created 'height' and 'weight' methods aka Accessor Methods

--- oop_ruby/oop_book.rb
+++ oop_ruby/oop_book.rb
@@ -1,18 +1,31 @@
+require "pry"
+
 class GoodDog
-  attr_accessor :name
+  attr_accessor :name, :weight, :height
+
+  def change_info(n,w,h)
+    name = n
+    weight = w
+    height = h
+  end
 
   def initialize(name)
     @name = name
   end
 
   def speak
-    "> #{@name} says Arf!!"
+    "> #{name} says Arf!!"
+  end
+
+  def info
+    "Name: #{name} | Weight: #{weight} | Height: #{height}"
   end
 end
 
 fido = GoodDog.new("Fido")
 jules = GoodDog.new("Jules")
 
+binding.pry
 puts fido.speak
 puts jules.speak
 puts fido.name
