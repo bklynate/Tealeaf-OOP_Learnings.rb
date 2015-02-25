ea84a6af5b84ad12729b9d71e1d5693b7af14837
bklynate <nathanielcfa@gmail.com>
2015-02-25 18:23:38 -0500

    small changes

--- oop_ruby/OOP_lecture_1_lesson.rb
+++ oop_ruby/OOP_lecture_1_lesson.rb
@@ -3,18 +3,34 @@
 #2. classes contain behaviors
 
 class Dog
-  def initialize
-    
+  def initialize(n,h,w)
+    @weight = w
+    @height = h
+    @name = n
   end
 
   def speak
-    puts "Bark!!"
+    @name + "Bark!!"
+  end
+
+  def get_name
+    @name 
+  end
+
+  def set_name=(new_name)
+    @name = new_name    
+  end
+  
+  def info
+    "#{@name} is #{@height} feet tall and weighs #{@weight} pounds."
   end
 end
 
-bruno = Dog.new
-bruno.speak
+bruno = Dog.new('bruno')
+puts bruno.speak
 
-fido = Dog.new
-fido.speak
+fido = Dog.new('fido')
+puts fido.speak
 
+puts fido.get_name
+puts bruno.get_name
