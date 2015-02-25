897464feba95a9902aced794cd8db91fe59706ff
bklynate <nathanielcfa@gmail.com>
2015-02-25 18:07:53 -0500

    Various practices followed along from the OOP Lecture One Video

---
+++ oop_ruby/OOP_lecture_1_lesson.rb
@@ -0,0 +1,16 @@
+#OOP in Ruby
+#1. classes and objects
+#2. classes contain behaviors
+
+class Dog
+  def speak
+    puts "Bark!!"
+  end
+end
+
+bruno = Dog.new
+teddy.speak
+
+fido = Dog.new
+fido.speak
+
