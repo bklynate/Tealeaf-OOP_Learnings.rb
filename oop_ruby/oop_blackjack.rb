bea75b3a668b5e97888238a1e8ee33114a9bebc6
bklynate <nathanielcfa@gmail.com>
2015-05-06 20:25:47 -0400

    Working player hand card and deck classes

---
+++ oop_ruby/oop_blackjack.rb
@@ -0,0 +1,115 @@
+class Card
+  attr_accessor :suit, :face_value
+  attr_reader :proper_suit_name
+  def initialize(suit,facevalue)
+    @suit = suit
+    @face_value = facevalue
+  end
+
+  def to_s
+    "#{proper_suit_name}[#{face_value}]"
+  end
+
+  def proper_suit_name
+    case suit
+      when 'H' then 'Hearts'
+      when 'S' then 'Spades'
+      when 'C' then 'Clubs'
+      when 'D' then 'Diamonds'
+    end 
+  end 
+end
+
+class Deck
+  attr_accessor :cards
+  def initialize
+    @cards = []
+    ['H','S','C','D'].each do |suit|
+      [2,3,4,5,6,7,8,9,10,"K","Q",'J','A'].each do |facevalue|
+        @cards << Card.new(suit,facevalue)
+      end
+    end
+    shuffle_cards!
+  end
+
+  def shuffle_cards!
+    cards.shuffle!
+  end
+
+  def deal_one
+    cards.pop
+  end
+end
+
+module Hand
+  def add_card(new_card)
+    cards << new_card   
+  end
+
+  def show_hand
+    puts "-----#{name}'s cards are-----"
+    puts "-----#{cards}-----"
+    puts "-----#{name}'s total: #{total}-----"
+  end
+
+  def total
+    face_values = cards.map{ |card| card.face_value }
+    total = 0
+
+    face_values.each do |card|
+      if card == 'A'
+        total += 11
+      elsif card.to_i == 0
+        total += 10
+      else
+        total += card
+      end
+    end
+
+    face_values.select { |card| card == 'A'}.count.times do
+      total -= 10 if total > 21
+    end
+
+    total
+  end
+
+  def is_busted?
+    total > 21
+  end
+
+end
+
+class Blackjack
+end
+
+class Human
+  include Hand
+  attr_accessor :cards
+  attr_reader :name
+  def initialize
+    puts "Hello player, What is your name?"
+    @name = gets.chomp.capitalize
+    @cards = []
+  end
+
+end
+
+class Dealer
+ include Hand
+ attr_accessor :cards
+ attr_reader :name
+  def initialize
+    @name = "Dealer"
+    @cards = []
+  end
+end
+
+nate = Human.new
+deck = Deck.new
+dealer = Dealer.new
+
+2.times do
+  nate.add_card(deck.deal_one)
+end
+
+puts nate.show_hand
